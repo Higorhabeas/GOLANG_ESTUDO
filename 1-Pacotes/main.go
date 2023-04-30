494bc88f02eea028884fa55ace214c7103ee42b3
Higor <higoras.silva0@gmail.com>
2023-04-30 16:26:58 -0300

    usando pacotes externos check emails

--- 1-Pacotes/main.go
+++ 1-Pacotes/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do main")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("higoras.silva0@gmail.com")
+	fmt.Println(erro)
 }
