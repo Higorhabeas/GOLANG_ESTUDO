1559ece36d2cb6a0f34f471e88355b0f126f43ca
Higor <higoras.silva0@gmail.com>
2023-06-04 15:09:16 -0300

    Teste unitario com cenários

--- 19-Testes Automatizados/19.1-Introducao/enderecos/endereco_test.go
+++ 19-Testes Automatizados/19.1-Introducao/enderecos/endereco_test.go
@@ -2,16 +2,36 @@ package enderecos
 
 import "testing"
 
-func TestTipoDeEndereco(t *testing.T) {
-	enderecoParaTeste := "avenida paulista"
+type cenarioDeTeste struct {
+	endetecoInserido string
+	retornoEsperado  string
+}
 
-	tipoDeEnderecoEsperado := "rua"
+func TestTipoDeEndereco(t *testing.T) {
+
+	cenariosDeTeste := []cenarioDeTeste{
+		{"rua 1", "rua"},
+		{"avenida 1", "avenida"},
+		{"rua 2", "rua"},
+		{"rodovia 3", "rua"},
+	}
+
+	for _, cenario := range cenariosDeTeste {
+		tipoDeEnderecoRecebido := TipoDoEndereco(cenario.endetecoInserido)
+		if tipoDeEnderecoRecebido != cenario.retornoEsperado {
+			t.Errorf("O tipo recebido %s é diferente do esperado %s", tipoDeEnderecoRecebido, cenario.retornoEsperado)
+		}
+	}
+
+	/*enderecoParaTeste := "Avenida paulista"
+
+	tipoDeEnderecoEsperado := "Avenida"
 
 	tipoDeEnderecoRecebido := TipoDoEndereco(enderecoParaTeste)
 
 	if tipoDeEnderecoRecebido != tipoDeEnderecoEsperado {
 		t.Errorf("O tipo recebido é diferente do esperado! Esperava %s e recebeu %s",
 			tipoDeEnderecoEsperado, tipoDeEnderecoRecebido)
-	}
+	}*/
 
 }
