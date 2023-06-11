ddcf4ff0173edb3ed4b18d0e6a54b178d5809533
Higor <higoras.silva0@gmail.com>
2023-06-11 09:54:19 -0300

    tentando ler oracle

--- lendo_oracle/lendo_oracle.go
+++ lendo_oracle/lendo_oracle.go
@@ -9,20 +9,17 @@ import (
 )
 
 func main() {
-
-	db, err := sql.Open("godror", "hr/hr@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME = XEPDB1)))")
+	db, err := sql.Open("godror", `user="hr" password="hr" connectString="localhost:1521/XEPDB1"`)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
 	var campoTabela string
-	err = db.QueryRow("SELECT department_name FROM departments where department_id=20").Scan(&campoTabela)
+	err = db.QueryRow("SELECT department_name FROM departments WHERE department_id = 20").Scan(&campoTabela)
 	if err != nil {
-		db.Close()
 		log.Fatal(err)
-
 	}
-	fmt.Println(campoTabela)
 
+	fmt.Println(campoTabela)
 }
